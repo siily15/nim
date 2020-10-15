31b6c43acc9162c5d4fd133d7b1c1073b255b8bd
siily <arnokosk15@gmail.com>
2020-10-15 13:40:43 +0300

    done

--- nim.py
+++ nim.py
@@ -1,24 +1,21 @@
 f = open ("nimsis0.txt", "r")
 n = f.readline()
 
-y = {
-
-}
-
+y = {}
+y2 = {}
+y3 = {}
 for x in range(int(n)):
     name = f.readline()
     splitted = name.split()
     y[splitted[0]] = splitted[1]
-print(y)
-
-y2 = {
-
-}
 
 for r in range(int(n)):
-    name = f.readline()
-    splitted2 = name.split()
+    name2 = f.readline()
+    splitted2 = name2.split()
     y2[splitted2[0]] = splitted2[1]
-print(y2)
 
-print(y + y2)
+for z in y:
+    for v in y2:
+        if z == v:
+            y3[y[z]] = y2[v]
+print(y3)
